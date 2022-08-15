4b5bf8b9bfdda1451ecab469b63fcd07ac3131cd
Marlon Dauernheimer <marlon.dauernheimer@gmail.com>
2022-08-15 12:19:09 -0300

    fix: adjusted link to remove text decoration in home

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,7 +4,7 @@ import AppName from '../components/AppName';
 
 const Home = () => {
 	return (
-		<h1>Welcome to <Link to="/wallet"><AppName/></Link></h1>
+		<h1>Welcome to <Link to="/wallet" className="text-decoration-none"><AppName/></Link></h1>
 	);
 }
 
