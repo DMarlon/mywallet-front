c59af60ae0a11782ddee657c8ea36cbea41b14b3
Marlon Dauernheimer <marlon.dauernheimer@gmail.com>
2022-08-15 21:13:37 -0300

    feat: logic created if not have parameter dont show button

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -6,11 +6,11 @@ const ButtonGroup = ({ labelAction, onClickAction, onClickClean, labelClean, toL
 
     return (
         <div className="d-grid gap-2 mt-2">
-            <Button variant="primary" className="text-white" size="lg" onClick={onClickAction}>{labelAction ?? "Action"}</Button>
-            <Button variant="secondary" size="lg" onClick={onClickClean}>{labelClean ?? "Clean"}</Button>
-            <Link to={toLink || "/"} className="col-12">
+            { onClickAction && <Button variant="primary" className="text-white" size="lg" onClick={onClickAction}>{labelAction ?? "Action"}</Button> }
+            { onClickClean && <Button variant="secondary" size="lg" onClick={onClickClean}>{labelClean ?? "Clean"}</Button>}
+            { toLink && <Link to={toLink || "/"} className="col-12">
                 <Button variant="secondary" size="lg" className="w-100">{labelLink ?? "Back"}</Button>
-            </Link>
+            </Link> }
         </div>
     );
 }
